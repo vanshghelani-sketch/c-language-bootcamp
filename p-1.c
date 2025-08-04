fcfebc5c83b2a30c87de3c34f5fa210a98e04533
vanshghelani-sketch <vanshghelani@gmail.com>
2025-08-04 19:58:18 +0530

    Add files via upload

---
+++ p-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+     printf("GHELANI VANSH \n");
+    
+    printf(" I AM 17 YEAR OLD \n");
+    
+     printf("I AM STUDY IN TAPI DIPLOMA ENGINEERIMG COLLEGE :");
+    
+   
+    
+    return 0;
+}
---
+++ p-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   printf("--------\n");
+   printf("|\t\t|\n");
+   printf("R\t\t|\n");
+   printf("N\t\t|\n");
+   printf("W\t\t|\n");
+   printf("|\t\t|\n");
+   printf("--------\n");
+   
+   
+   
+   
+    
+    return 0;
+}
